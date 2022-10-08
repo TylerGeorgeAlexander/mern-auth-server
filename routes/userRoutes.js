d6b0644872f2dfea7f43e9dc77a292dd03f7fdd5
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-10-07 19:47:55 -0700

    Creating the logout route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
-const passport = require("passport")
-const jwt = require("jsonwebtoken")
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
 
 const {
   getToken,
@@ -13,8 +13,8 @@ const {
 
 // ME - Endpoint to fetch user details
 router.get("/me", verifyUser, (req, res, next) => {
-  res.send(req.user)
-})
+  res.send(req.user);
+});
 
 // SIGNUP
 router.post("/signup", (req, res, next) => {
@@ -56,75 +56,106 @@ router.post("/signup", (req, res, next) => {
 
 // LOGIN
 router.post("/login", passport.authenticate("local"), (req, res, next) => {
-  const token = getToken({ _id: req.user._id })
-  const refreshToken = getRefreshToken({ _id: req.user._id })
+  const token = getToken({ _id: req.user._id });
+  const refreshToken = getRefreshToken({ _id: req.user._id });
   User.findById(req.user._id).then(
-    user => {
-      user.refreshToken.push({ refreshToken })
+    (user) => {
+      user.refreshToken.push({ refreshToken });
       user.save((err, user) => {
         if (err) {
-          res.statusCode = 500
-          res.send(err)
+          res.statusCode = 500;
+          res.send(err);
         } else {
-          res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS)
-          res.send({ success: true, token })
+          res.cookie("refreshToken", refreshToken, COOKIE_OPTIONS);
+          res.send({ success: true, token });
         }
-      })
+      });
     },
-    err => next(err)
-  )
-})
+    (err) => next(err)
+  );
+});
+
+// LOGOUT
+router.get("/logout", verifyUser, (req, res, next) => {
+  const { signedCookies = {} } = req;
+  const { refreshToken } = signedCookies;
+  User.findById(req.user._id).then(
+    (user) => {
+      const tokenIndex = user.refreshToken.findIndex(
+        (item) => item.refreshToken === refreshToken
+      );
+
+      if (tokenIndex !== -1) {
+        user.refreshToken.id(user.refreshToken[tokenIndex]._id).remove();
+      }
+
+      user.save((err, user) => {
+        if (err) {
+          res.statusCode = 500;
+          res.send(err);
+        } else {
+          res.clearCookie("refreshToken", COOKIE_OPTIONS);
+          res.send({ success: true });
+        }
+      });
+    },
+    (err) => next(err)
+  );
+});
 
 // REFRESH TOKEN
 router.post("/refreshToken", (req, res, next) => {
-  const { signedCookies = {} } = req
-  const { refreshToken } = signedCookies
+  const { signedCookies = {} } = req;
+  const { refreshToken } = signedCookies;
 
   if (refreshToken) {
     try {
-      const payload = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET)
-      const userId = payload._id
+      const payload = jwt.verify(
+        refreshToken,
+        process.env.REFRESH_TOKEN_SECRET
+      );
+      const userId = payload._id;
       User.findOne({ _id: userId }).then(
-        user => {
+        (user) => {
           if (user) {
             // Find the refresh token against the user record in database
             const tokenIndex = user.refreshToken.findIndex(
-              item => item.refreshToken === refreshToken
-            )
+              (item) => item.refreshToken === refreshToken
+            );
 
             if (tokenIndex === -1) {
-              res.statusCode = 401
-              res.send("Unauthorized")
+              res.statusCode = 401;
+              res.send("Unauthorized");
             } else {
-              const token = getToken({ _id: userId })
+              const token = getToken({ _id: userId });
               // If the refresh token exists, then create new one and replace it.
-              const newRefreshToken = getRefreshToken({ _id: userId })
-              user.refreshToken[tokenIndex] = { refreshToken: newRefreshToken }
+              const newRefreshToken = getRefreshToken({ _id: userId });
+              user.refreshToken[tokenIndex] = { refreshToken: newRefreshToken };
               user.save((err, user) => {
                 if (err) {
-                  res.statusCode = 500
-                  res.send(err)
+                  res.statusCode = 500;
+                  res.send(err);
                 } else {
-                  res.cookie("refreshToken", newRefreshToken, COOKIE_OPTIONS)
-                  res.send({ success: true, token })
+                  res.cookie("refreshToken", newRefreshToken, COOKIE_OPTIONS);
+                  res.send({ success: true, token });
                 }
-              })
+              });
             }
           } else {
-            res.statusCode = 401
-            res.send("Unauthorized")
+            res.statusCode = 401;
+            res.send("Unauthorized");
           }
         },
-        err => next(err)
-      )
+        (err) => next(err)
+      );
     } catch (err) {
-      res.statusCode = 401
-      res.send("Unauthorized")
+      res.statusCode = 401;
+      res.send("Unauthorized");
     }
   } else {
-    res.statusCode = 401
-    res.send("Unauthorized")
+    res.statusCode = 401;
+    res.send("Unauthorized");
   }
-})
+});
 
 module.exports = router;
