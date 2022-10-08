9db9db39c884ae2fb40ee46694727a3296e2d206
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-10-07 20:20:02 -0700

    add the submit handler to the registration form

--- mern-auth-client/src/components/Register.jsx
+++ mern-auth-client/src/components/Register.jsx
@@ -1,20 +1,66 @@
-import { Button, FormGroup, InputGroup } from "@blueprintjs/core";
-import React, { useState } from "react";
+import { Button, Callout, FormGroup, InputGroup } from "@blueprintjs/core"
+import React, { useContext, useState } from "react"
+import { UserContext } from "./context/UserContext"
 
 const Register = () => {
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [error, setError] = useState("")
+  const [firstName, setFirstName] = useState("")
+  const [lastName, setLastName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const [userContext, setUserContext] = useContext(UserContext)
+
+  const formSubmitHandler = e => {
+    e.preventDefault()
+    setIsSubmitting(true)
+    setError("")
+
+    const genericErrorMessage = "Something went wrong! Please try again later."
+
+    fetch(process.env.REACT_APP_API_ENDPOINT + "users/signup", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ firstName, lastName, username: email, password }),
+    })
+      .then(async response => {
+        setIsSubmitting(false)
+        if (!response.ok) {
+          if (response.status === 400) {
+            setError("Please fill all the fields correctly!")
+          } else if (response.status === 401) {
+            setError("Invalid email and password combination.")
+          } else if (response.status === 500) {
+            console.log(response)
+            const data = await response.json()
+            if (data.message) setError(data.message || genericErrorMessage)
+          } else {
+            setError(genericErrorMessage)
+          }
+        } else {
+          const data = await response.json()
+          setUserContext(oldValues => {
+            return { ...oldValues, token: data.token }
+          })
+        }
+      })
+      .catch(error => {
+        setIsSubmitting(false)
+        setError(genericErrorMessage)
+      })
+  }
 
   return (
     <>
-      <form className="auth-form">
+      {error && <Callout intent="danger">{error}</Callout>}
+
+      <form onSubmit={formSubmitHandler} className="auth-form">
         <FormGroup label="First Name" labelFor="firstName">
           <InputGroup
             id="firstName"
             placeholder="First Name"
-            onChange={(e) => setFirstName(e.target.value)}
+            onChange={e => setFirstName(e.target.value)}
             value={firstName}
           />
         </FormGroup>
@@ -22,7 +68,7 @@ const Register = () => {
           <InputGroup
             id="lastName"
             placeholder="Last Name"
-            onChange={(e) => setLastName(e.target.value)}
+            onChange={e => setLastName(e.target.value)}
             value={lastName}
           />
         </FormGroup>
@@ -31,7 +77,7 @@ const Register = () => {
             id="email"
             type="email"
             placeholder="Email"
-            onChange={(e) => setEmail(e.target.value)}
+            onChange={e => setEmail(e.target.value)}
             value={email}
           />
         </FormGroup>
@@ -40,14 +86,20 @@ const Register = () => {
             id="password"
             placeholder="Password"
             type="password"
-            onChange={(e) => setPassword(e.target.value)}
+            onChange={e => setPassword(e.target.value)}
             value={password}
           />
         </FormGroup>
-        <Button intent="primary" text="Register" fill type="submit" />
+        <Button
+          intent="primary"
+          disabled={isSubmitting}
+          text={`${isSubmitting ? "Registering" : "Register"}`}
+          fill
+          type="submit"
+        />
       </form>
     </>
-  );
-};
+  )
+}
 
-export default Register;
+export default Register
