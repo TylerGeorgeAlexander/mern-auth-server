c7b50966e7335c82253f6840da73a5bb6ae68b54
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-10-07 17:55:58 -0700

    added LocalStrat and formatting

--- models/user.js
+++ models/user.js
@@ -1,14 +1,14 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-const passportLocalMongoose = require("passport-local-mongoose")
+const passportLocalMongoose = require("passport-local-mongoose");
 
 const Session = new Schema({
   refreshToken: {
     type: String,
     default: "",
   },
-})
+});
 
 const User = new Schema({
   firstName: {
@@ -30,16 +30,16 @@ const User = new Schema({
   refreshToken: {
     type: [Session],
   },
-})
+});
 
 //Remove refreshToken from the response
 User.set("toJSON", {
   transform: function (doc, ret, options) {
-    delete ret.refreshToken
-    return ret
+    delete ret.refreshToken;
+    return ret;
   },
-})
+});
 
-User.plugin(passportLocalMongoose)
+User.plugin(passportLocalMongoose);
 
-module.exports = mongoose.model("User", User)
+module.exports = mongoose.model("User", User);
