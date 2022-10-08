24d9a42787eb436c63c00bc07e1a9b0559f873ac
Tyler Alexander <TylerGeorgeAlexander@gmail.com>
2022-10-07 20:23:09 -0700

    refresh token endpoint

--- mern-auth-client/src/App.js
+++ mern-auth-client/src/App.js
@@ -1,12 +1,41 @@
 import { Card, Tab, Tabs } from "@blueprintjs/core";
-import { useState } from "react";
-import Login from "./components/Login";
-import Register from "./components/Register";
+import { useCallback, useContext, useEffect, useState } from "react";
+import { UserContext } from "./context/UserContext";
+import Loader from "./Loader";
+import Login from "./Login";
+import Register from "./Register";
+import Welcome from "./Welcome";
 
 function App() {
   const [currentTab, setCurrentTab] = useState("login");
+  const [userContext, setUserContext] = useContext(UserContext);
 
-  return (
+  const verifyUser = useCallback(() => {
+    fetch(process.env.REACT_APP_API_ENDPOINT + "users/refreshToken", {
+      method: "POST",
+      credentials: "include",
+      headers: { "Content-Type": "application/json" },
+    }).then(async (response) => {
+      if (response.ok) {
+        const data = await response.json();
+        setUserContext((oldValues) => {
+          return { ...oldValues, token: data.token };
+        });
+      } else {
+        setUserContext((oldValues) => {
+          return { ...oldValues, token: null };
+        });
+      }
+      // call refreshToken every 5 minutes to renew the authentication token.
+      setTimeout(verifyUser, 5 * 60 * 1000);
+    });
+  }, [setUserContext]);
+
+  useEffect(() => {
+    verifyUser();
+  }, [verifyUser]);
+
+  return userContext.token === null ? (
     <Card elevation="1">
       <Tabs id="Tabs" onChange={setCurrentTab} selectedTabId={currentTab}>
         <Tab id="login" title="Login" panel={<Login />} />
@@ -14,6 +43,10 @@ function App() {
         <Tabs.Expander />
       </Tabs>
     </Card>
+  ) : userContext.token ? (
+    <Welcome />
+  ) : (
+    <Loader />
   );
 }
 
